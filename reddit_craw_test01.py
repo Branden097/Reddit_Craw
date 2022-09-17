e517e990a53029f8eefc0a035bb0c9767ef28b3a
Branden <105219797+Branden097@users.noreply.github.com>
2022-09-17 12:19:24 +0800

    Update reddit_craw_test01.py

--- reddit_craw_test01.py
+++ reddit_craw_test01.py
@@ -36,7 +36,7 @@ def search_reddit(inputname):
     url = f"https://www.reddit.com/search/?q={inputname}"
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    '''
+    
     time.sleep(5)
     js = " return action=document.body.scrollHeight " #初始化現在滾動條所在高度為0 
     height = 0#當前窗口總高度
@@ -51,20 +51,17 @@ def search_reddit(inputname):
         new_height = driver.execute_script(js)
     
     time.sleep(5)
-    '''
+    
     soup = bs(driver.page_source, 'lxml')
 
-    post_or_crosspost = driver.find_element(by=By.CSS_SELECTOR,value='._2fCzxBE1dlMh4OFc7B3Dun')
+    # post_or_crosspost = driver.find_element(by=By.CSS_SELECTOR,value='._2fCzxBE1dlMh4OFc7B3Dun')
     for i in soup.select('div > div > div._2n04GrCyhhQf-Kshn7akmH._19FzInkloQSdrf0rh3Omen > div:nth-child(1) > div > div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE > a'):
-        if post_or_crosspost.text != 'Corssposted by':
-            n = i.get("href")
-            addr = 'https://www.reddit.com'+ n
-            address_list.append(addr)
+        n = i.get("href")
+        addr = 'https://www.reddit.com'+ n
+        address_list.append(addr)
 
     driver.quit()
-    print(address_list)
 
-    '''
     for url in address_list:
         headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'}
         res = requests.get(url, headers=headers)
@@ -74,42 +71,43 @@ def search_reddit(inputname):
         url2 = url + '.json'
         res2 = requests.get(url2, headers=headers).json()
         
+        url_list.append(str(url))#文章網址
 
-        #文章網址
-        url_list.append(url)
-
-        # 標題
-        for i in soup.select(f'#t3_{id} ._eYtD2XCVieq6emjKBH3m'):
-            title_list.append(i.text.replace('\n',' '))
-
-        # 時間
-        unix_time = res2[0]['data']['children'][0]['data']['created']
+        unix_time = res2[0]['data']['children'][0]['data']['created']# 時間
         real_time = datetime.fromtimestamp(unix_time)
         time_list.append(str(real_time))
 
-        # # 文章
+        # 標題
+        for i in soup.select(f'#t3_{id} ._29WrubtjAcKqzJSPdQqQ4h ._eYtD2XCVieq6emjKBH3m'):
+            title_list.append(i.text.replace('\n',' '))
+
+        # 文章
         # for i in soup.select(f'#t3_{id} ._1qeIAgB0cPwnLhDF9XSiJM'):
         #     content_list.append(i.text)
 
         # 網址
-        for i in soup.select(f'#t3_{id} ._10wC0aXnrUKfdJ4Ssz-o14'):
-            if i:
+        urll = soup.select(f'#t3_{id} ._10wC0aXnrUKfdJ4Ssz-o14')
+        if len(urll) != 0:
+            for i in urll:
                 content_url.append(i.select_one('a').get('href'))
-            else:
-                content_url.append('0')
+        else:
+            content_url.append('None')
 
         # 圖片
-        for i in soup.select(f'#t3_{id} ._1NSbknF8ucHV2abfCZw2Z1', limit=2):
-            if i:
+        img = soup.select(f'#t3_{id} ._1NSbknF8ucHV2abfCZw2Z1', limit=2)
+        if len(img) != 0:
+            for i in img:
                 content_img.append(i.select_one('img').get('src'))
-            else:
-                content_img.append('0')
+        else:
+            content_img.append('None')
+
         # 影片
-        for i in soup.select(f'#t3_{id} .m3aNC6yp8RrNM_-a0rrfa._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4._3PIKVMCKdveCEcyiKr43sU'):
-            if i:
+        video = soup.select(f'#t3_{id} .m3aNC6yp8RrNM_-a0rrfa._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4._3PIKVMCKdveCEcyiKr43sU')
+        if len(video) != 0:
+            for i in video:
                 content_video.append(i.select_one('source').get('src'))
-            else:
-                content_video.append('0')
+        else:
+            content_video.append('None')
 
     d = {}
     d['title'] = title_list
@@ -119,8 +117,8 @@ def search_reddit(inputname):
     d['content_img'] = content_img
     d['content_video'] = content_video
     df = pd.DataFrame(d)
+    # print(df)
     df.to_excel(inputname+'_content.xlsx', encoding="utf_8_sig")
-    '''
 
 search_reddit('Andy Lyon')
-# search_reddit('Injury')
+search_reddit('Injury')
